03522079aec93a23980abf8784d3b0bc4fce8435
Crunchitect <95293374+Crunchitect@users.noreply.github.com>
2024-03-17 20:04:32 +0700

    refactored astar

--- modules/astar.py
+++ modules/astar.py
@@ -1,156 +1,22 @@
-import cv2
-import numpy as np
-import time
+import cv2, numpy as np
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
 
-curr_nodes = []
-filename = 'assets/map.png'
-src = cv2.imread(cv2.samples.findFile(filename))
-def update_node(path):
-    global curr_nodes
-    curr_nodes = path
-    # print(curr_nodes)
+def get_path(maze, start, end):
+    matrix = cv2.cvtColor(maze, cv2.COLOR_BGR2GRAY)
+    (_, matrix) = cv2.threshold(matrix, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    matrix = cv2.bitwise_not(matrix).transpose().tolist()
+    grid = Grid(matrix=matrix)
 
-    dst = cv2.ximgproc.thinning(cv2.bitwise_not(cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)))
-    dst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
-    for i in curr_nodes:
-        dst = cv2.circle(dst, (i.position[1], i.position[0]), 1, (0, 0, 255), 1)
-    cv2.imshow('maze', dst)
-    cv2.waitKey(1)
+    # start = grid.node(104, 45)
+    # end = grid.node(98, 434)
 
+    startx = grid.node(*(start[::-1]))
+    endx = grid.node(*(end[::-1]))
 
-class Node:
-    """A node class for A* Pathfinding"""
+    finder = AStarFinder(diagonal_movement=DiagonalMovement.only_when_no_obstacle)
+    path, runs = finder.find_path(startx, endx, grid)
 
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
-        self.g = 0
-        self.h = 0
-        self.f = 0
-
-    def __eq__(self, other):
-        return self.position == other.position
-
-    def __repr__(self):
-        return f'Node({self.position})'
-
-
-def astar(maze, start, end):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
-
-    # Create start and end node
-    start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, end)
-    end_node.g = end_node.h = end_node.f = 0
-
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
-
-    # Add the start node
-    open_list.append(start_node)
-
-    # Loop until you find the end
-    while len(open_list) > 0:
-        # Get the current node
-        current_node = open_list[0]
-        current_index = 0
-        # if __name__ == '__main__':
-        update_node(closed_list)
-
-        for index, item in enumerate(open_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
-
-        # Pop current off open list, add to closed list
-        open_list.pop(current_index)
-        closed_list.append(current_node)
-
-        # Found the goal
-        if current_node == end_node:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1] # Return reversed path
-
-        # Generate children
-        children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
-
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
-                continue
-
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] != 0:
-                continue
-
-            # Create new node
-            new_node = Node(current_node, node_position)
-
-            # Append
-            children.append(new_node)
-
-        # Loop through children
-        for child in children:
-
-            # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
-                    continue
-
-            # Create the f, g, and h values
-            child.g = current_node.g + 1
-            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
-            child.f = child.g + child.h
-
-            # Child is already in the open list
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    continue
-
-            # Add the child to the open list
-            open_list.append(child)
-
-def preprocess(img):
-    return cv2.bitwise_not(cv2.ximgproc.thinning(cv2.bitwise_not(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))))
-
-def get_path(img, start, end):
-    maze = preprocess(img)
-    path = astar(maze, start[::-1], end[::-1])
-    return path
-
-def main():
-    maze = cv2.ximgproc.thinning(cv2.bitwise_not(cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)))
-    maze = cv2.bitwise_not(maze)
-    # maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    #         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    #         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    #         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    #         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    #         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    #         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    #         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    #         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
-    start = (309, 0)
-    end = (189, 747)
-    # start = (0, 0)
-    # end = (100, 100)
-    start_time = time.perf_counter()
-    path = astar(maze, start, end)
-    end_time = time.perf_counter()
-    print(path, end_time - start_time)
-
-
-if __name__ == '__main__':
-    main()
+    print('operations:', runs, 'path length:', len(path))
+    return np.array([[node.x, node.y] for node in path])
--- scrap.py
+++ scrap.py
@@ -1,29 +1,26 @@
 from modules.get_junctions import get_junctions
 from modules.astar import get_path
-import cv2, numpy as np, math
+from functools import cache
+import cv2, numpy as np
 
-
-def dist(p1: list[int, int], p2: list[int, int]):
-    return math.hypot(p2[0] - p1[0], p2[1] - p1[1])
-
-
-def cross_product(v1: list[int, int], v2: list[int, int]):
+@cache
+def cross_product(v1: tuple[int, int], v2: tuple[int, int]):
     return (v1[0] * v2[1]) - (v1[1] * v2[0])
 
 
 path_str = "cross();\n"
 maze = cv2.imread('assets/real.png')
-path = np.array(get_path(maze, (45, 104), (482, 190)))
+path = get_path(maze, (45, 104), (434, 98))
 junctions = get_junctions(maze)
-# print(junctions)
+i = 0
 for x1, y1, x2, y2, x3, y3 in np.lib.stride_tricks.sliding_window_view(path.flatten(), (6,))[::2]:
+    print(i)
+    i += 1
     for junction_type, positions in junctions.items():
         for position in positions:
-            is_intersection = (x2 == position[0] and y2 == position[1])
-            in_between_intersection = (dist((x2, y2), position) == 1 and dist((x3, y3), position) == 1)
-            if is_intersection or in_between_intersection:
-                u = [x2 - x1, y2 - y1]
-                v = [x3 - x2, y3 - y2]
+            if x2 == position[0] and y2 == position[1]:
+                u = (x2 - x1, y2 - y1)
+                v = (x3 - x2, y3 - y2)
                 output = cross_product(u, v)
                 if output > 0:
                     path_str += "\tleft(); cross();\n"
